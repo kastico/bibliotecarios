d237cb21bece841d9ba97775acd6d0c7e07725fe
dleitao <diomlgomes@gmail.com>
2023-09-21 12:30:47 +0100

    func inserir livro add

--- bibliotecariosproj1/main.c
+++ bibliotecariosproj1/main.c
@@ -10,3 +10,28 @@ printf("Olla");
 return 0;
 
 }
+
+
+void inserir_livro(t_livro *p){
+
+
+    printf("Introduza o título do livro: ");
+    fflush(stdin);
+    gets(p->titulo);
+
+    printf("\nIntroduza o autor do livro: ");
+    fflush(stdin);
+    gets(p->.autor);
+
+    do {
+
+        printf("\nIntroduza o ano de publicação do livro: ");
+        scanf("%d", p->ano);
+
+    }while(p->ano < 1500 && p-> ano > 2023);
+
+
+    printf("\nIntroduza a editora do livro: ");
+    fflush(stdin);
+    gets(p->editora);
+}
