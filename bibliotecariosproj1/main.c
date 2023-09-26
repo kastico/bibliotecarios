d4ff3af9a42fdf4cd014be018b37ca9928513bc7
kastico <antonio_barbosa00@live.com.pt>
2023-09-26 09:08:06 +0100

    Versao Final

--- bibliotecariosproj1/main.c
+++ bibliotecariosproj1/main.c
@@ -1,8 +1,11 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <locale.h>
 #define MAX 10
 #define MAX_STR 40
+#define ANO_ACTUAL 2023
 
 typedef struct{
     char titulo[MAX_STR];
@@ -14,32 +17,36 @@ typedef struct{
 
 
 void print_livro(t_livro livro){
-    printf("Título: %s\tAutor: %s\nEditora: %s\t Ano: %d", livro.titulo, livro.autor, livro.editora, livro.ano);
+    printf("\n\nTítulo: %s\tAutor: %s\nEditora: %s\tAno: %d", livro.titulo, livro.autor, livro.editora, livro.ano);
 }
 
 void print_matriz(t_livro livros[], int n){
 
+
     for(int i=0;i<n;i++)
+    {
         print_livro(livros[i]);
+    }
+
 }
 
 void le_ficheiro(FILE *fic,t_livro *livros,int *n){
 
-    int ano;
-    char tit[MAX],aut[MAX],edi[MAX];
+    int an;
+    char tit[MAX_STR],aut[MAX_STR],edi[MAX_STR];
 
-    while( (fscanf(fic,"%[^#]#%[^#]#%d#%[^#]",tit,aut,&ano,edi))!=EOF )
+    while( (fscanf(fic,"%[^#]#%[^#]#%d#%s\n",tit,aut,&an,edi))!=EOF )
     {
         strcpy(livros->titulo,tit);
         strcpy(livros->autor,aut);
-        livros->ano=ano;
+        livros->ano=an;
         strcpy(livros->editora,edi);
-        n++;
+        (*n)++;
         livros++;
     }
 }
 
-void inserir_livro(t_livro *p){
+void inserir_livro(t_livro *p, int *n){
 
 
     printf("Introduza o título do livro: ");
@@ -55,95 +62,75 @@ void inserir_livro(t_livro *p){
         printf("\nIntroduza o ano de publicação do livro: ");
         scanf("%d", &p->ano);
 
-    }while(p->ano < 1500 && p-> ano > 2023);
+    }while(p->ano < 1500 || p-> ano > ANO_ACTUAL);
 
 
     printf("\nIntroduza a editora do livro: ");
     fflush(stdin);
     gets(p->editora);
+    (*n)++;
 }
 
-FILE *abrir_ficheiro(char tipo[],char url[])
+FILE *abrir_ficheiro(char tipo)
 {
-
     FILE *fic;
-    char op='2';
 
-    do
-    {
-        if (strlen(url)!=0)
-        {
-
-            printf("\n1- Manter ficheiro \"%s\"",url);
-            printf("\n2- Novo\n");
-            while ((op=getchar())!= '1' && op!= '2');
-        }
-        if(op == '2')
-        {
-            printf("\nNome do ficheiro: ");
-            fflush(stdin);
-            gets(url);
-        }
-        fic = fopen(url,tipo);
-        if(fic == NULL)
-        {
-            system("cls");
-            printf("Erro, nao foi possivel abrir o arquivo\n");
-            printf("\nTentar novamente? 1- sim 0- nao  ");
-            fflush(stdin);
-            op=getchar();
-            if(op != '1')
-                return NULL;
-        }
-        else
-        {
-            printf("\n Ficheiro: %s guardado com sucesso",url);
-            return fic;
-        }
-    }
-    while (fic==NULL);
+    fic = fopen("livros.txt",&tipo);
+    return fic;
 
 }
 
-void escreve_ficheiro(FILE *fic, t_livro *livros,int n)
+void escreve_ficheiro( t_livro livros[],int n)
 {
-
+    FILE *fic;
+    fic = fopen("livros.txt","w");
+    printf("FILE:  %s ",fic);
     for (int i=0; i<n; i++)
     {
-
-        fprintf (fic, "%s#%s#%d#%s", livros->titulo, livros->autor, livros->ano, livros->editora);
-
-        livros++;
+        fprintf(fic, "%s#%s#%d#%s\n", livros[i].titulo, livros[i].autor, livros[i].ano, livros[i].editora);
     }
+    fclose(fic);
 }
 
-
-void main ()
+int main ()
 
 {
+    setlocale(LC_ALL,"PORTUGUESE");
     int n_livros;
     t_livro livros[MAX];
+    FILE *fic;
     int op;
+    fic=abrir_ficheiro('r');
+    le_ficheiro(fic, livros, &n_livros);
+    fclose(fic);
+
+
     do{
-    system("cls");
+   // system("cls");
     printf("\n*** ** Gestao de livros ** ***\n\n");
     printf("\n1- Lista de livros");
-    printf("\n2- Atualizar lista de livros");
+    printf("\n2- Inserir novo livro");
     printf("\n3- Sair\n");
-    scanf ("%d",&op);
-    }while((op=getchar())!= '1' && op != '2' && op != '3');
+    do
+    {
+        scanf ("%d",&op);
+    }while(op>3 || op<1);
 
-    switch (op);
+    switch (op)
+        {
+            case 3:
+                escreve_ficheiro( livros, n_livros);
+                break;
+            case 1:
+                print_matriz(livros, n_livros);
+            break;
+            case 2:
+                inserir_livro(&livros[n_livros], &n_livros);
+            break;
+        }
+    }while(op!=3);
 
-        case 3:
-        break;
 
-        case 1:
-        break;
 
-        case 2:
-        break;
-
-    return op;
-
+return 0;
 }
